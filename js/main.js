ddf45ba381d67bc0987b6fe199d368c3bfbeb47d
BLITzStudio <81566145+BLITzStudio@users.noreply.github.com>
2021-09-08 15:55:27 +0300

    "nothing more added"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-const navbar = document.querySelector("nav");
-function sc() {}
-sc();
+const svg = document.getElementsByClassName("logo");
+console.log(svg);
+const colors = ["red", "blue", "green", "yellow", "#03045e"];
+
+function click() {
+  const rando = () => {
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+  document.documentElement.style.cssText = `--ellipse:${rando()};
+  --rectangle:${rando()};`;
+  console.log(1);
+}
