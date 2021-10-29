dc61fa55ce208357f2332fe2ecfeed97ecca7753
BLITzStudio <81566145+BLITzStudio@users.noreply.github.com>
2021-10-29 21:45:07 +0300

    share btn added in dad jokes page

--- pages/dad_Jokes/app.js
+++ pages/dad_Jokes/app.js
@@ -1,27 +1,31 @@
 const p = document.querySelector("p");
-const buttons = document.querySelectorAll("button");
-
+const get_JokeBtn = document.querySelectorAll("#get_JokeBtn");
+const share = document.querySelector("#share");
+var data;
 const config = {
   headers: { Accept: "application/json" },
 };
 const get_Joke = async function () {
   const req = await axios.get(" https://icanhazdadjoke.com/", config);
-  const data = req.data.joke;
-  console.log(req);
+  data = req.data;
   return data;
 };
 
 const add_Joke = async function () {
   const joke = await get_Joke();
-  p.innerText = joke;
+  p.innerText = joke.joke;
 };
 
-for (const button in buttons) {
-}
-
-buttons.forEach((button) => {
+get_JokeBtn.forEach((button) => {
   button.addEventListener("click", async function () {
     await add_Joke();
+  });
+});
+share.addEventListener("click", async function () {
+  navigator.share({
+    title: "Dad_Joke",
+    text: "Check out this joke ",
+    url: `https://icanhazdadjoke.com/j/${data.id}`,
   });
 });
 add_Joke();
